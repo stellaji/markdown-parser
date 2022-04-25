ca9b59882feea1cb063e629141687179c29b94ed
Stella Ji <stellaji0823@gmail.com>
2022-04-24 23:35:45 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,7 +11,8 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
+        int tempIndex = 0;
+        while(currentIndex < markdown.length() - 1) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
@@ -20,9 +21,11 @@ public class MarkdownParse {
             if (openParen == -1 || closeParen == -1) {
                 return toReturn;
             }
-            else {
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if(tempIndex > currentIndex) {
+                break;
             }
+            tempIndex = currentIndex;
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
 
