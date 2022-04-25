03292ac98b48ddf864d704707630186578a7a56b
Stella Ji <stellaji0823@gmail.com>
2022-04-24 22:41:27 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,7 +16,13 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            //add if statement here to make program run even if there is no closing )
+            if (openParen == -1 || closeParen == -1) {
+                return toReturn;
+            }
+            else {
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
             currentIndex = closeParen + 1;
         }
 
